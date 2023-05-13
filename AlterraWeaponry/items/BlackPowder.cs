d387e7ab089ee2ec7715f23e58901859fc8ae945
V E L Δ <36549174+VELD-Dev@users.noreply.github.com>
2023-05-13 17:58:01 +0200

    Fixed a few stuff, miracly working half

--- AlterraWeaponry/items/BlackPowder.cs
+++ AlterraWeaponry/items/BlackPowder.cs
@@ -13,16 +13,19 @@ internal class BlackPowder
 
     public BlackPowder()
     {
-        SetupGameObject();
+        Main.logger.LogDebug("Loading BlackPowder prefab info");
         this.Info = PrefabInfo
             .WithTechType(classId: ClassID, displayName: null, description: null, unlockAtStart: true, techTypeOwner: Assembly.GetExecutingAssembly())
             .WithIcon(Main.assets.LoadAsset<Sprite>("Sprite.BlackPowder"))
             .WithSizeInInventory(new(1, 1));
         TechType = this.Info.TechType;
+        SetupGameObject();
+        Main.logger.LogDebug("Loaded BlackPowder prefab info and assigned TechType");
     }
 
     public void Patch()
     {
+        Main.logger.LogDebug("Loading BlackPowder recipe");
         RecipeData recipe = new()
         {
             craftAmount = 1,
@@ -34,8 +37,9 @@ internal class BlackPowder
             }
         };
 
-        CustomPrefab customPrefab = new(this.Info);
+        Main.logger.LogDebug("Loaded BlackPowder recipe, loading custom prefab...");
 
+        CustomPrefab customPrefab = new(this.Info);
 
         customPrefab.SetGameObject(AssetPrefab);
         customPrefab.SetEquipment(EquipmentType.None);
@@ -45,6 +49,8 @@ internal class BlackPowder
             .WithStepsToFabricatorTab("Resources", "BasicMaterials");
 
         customPrefab.Register();
+
+        Main.logger.LogDebug("Loaded and registered BlackPowder prefab.");
     }
 
     /// <summary>
@@ -53,19 +59,29 @@ internal class BlackPowder
     /// <returns>the final version of the gameobject</returns>
     public GameObject SetupGameObject()
     {
-        Pickupable pickupable = AssetPrefab.GetComponent<Pickupable>();
+        Main.logger.LogDebug("Setting up BlackPowder GameObject.");
+        Main.logger.LogDebug("Setting pickupable component.");
+        Pickupable pickupable = AssetPrefab.EnsureComponent<Pickupable>();
+        Main.logger.LogDebug("Overriding Pickupable TechType");
         pickupable.overrideTechType = TechType;
 
-        PrefabIdentifier prefabIdentifier = AssetPrefab.GetComponent<PrefabIdentifier>();
+        Main.logger.LogDebug("Set up pickupable component. Now setting PrefabIdentifier");
+
+        PrefabIdentifier prefabIdentifier = AssetPrefab.EnsureComponent<PrefabIdentifier>();
         prefabIdentifier.ClassId = ClassID;
         prefabIdentifier.name = ClassID;
 
-        TechTag techTag = AssetPrefab.GetComponent<TechTag>();
+        Main.logger.LogDebug("Set up PrefabIdentifier component. Now setting TechTag");
+
+        TechTag techTag = AssetPrefab.EnsureComponent<TechTag>();
         techTag.type = TechType;
+
+        Main.logger.LogDebug("Set up TechTag component. Instantiating Object...");
 
         AssetPrefab.SetActive(true);
 
         GameObject go = UnityEngine.Object.Instantiate<GameObject>(AssetPrefab);
+        Main.logger.LogDebug("Object correctly instantiated. Set up Black Powder correctly");
         return go;
     }
 }
--- AlterraWeaponry/items/Coal.cs
+++ AlterraWeaponry/items/Coal.cs
@@ -10,16 +10,19 @@ internal class Coal
 
     public Coal()
     {
+        Main.logger.LogDebug("Loading Coal prefab info");
         this.Info = PrefabInfo
             .WithTechType(classId: ClassID, displayName: null, description: null, unlockAtStart: true, techTypeOwner: Assembly.GetExecutingAssembly())
             .WithIcon(Main.assets.LoadAsset<Sprite>("Sprite.Coal"))
             .WithSizeInInventory(new(1, 1));
 
         TechType = this.Info.TechType;
+        Main.logger.LogDebug("Loaded Coal prefab and assigned techType");
     }
 
     public void Patch()
     {
+        Main.logger.LogDebug("Setting Coal recipe");
         RecipeData recipe = new()
         {
             craftAmount = 4,
@@ -28,6 +31,8 @@ internal class Coal
                 new(TechType.CreepvinePiece, 1)
             }
         };
+
+        Main.logger.LogDebug("Recipe set, now patching prefab.");
 
         CustomPrefab customPrefab = new(this.Info);
         PrefabTemplate clone = new CloneTemplate(this.Info, TechType.Nickel);
@@ -40,5 +45,7 @@ internal class Coal
             .WithStepsToFabricatorTab("Resources", "BasicMaterials");
 
         customPrefab.Register();
+
+        Main.logger.LogDebug("Prefab loaded and registered for Coal.");
     }
 }
